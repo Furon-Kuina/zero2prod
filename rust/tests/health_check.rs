843251f1e1d6b0872ae6aaae5659287340fbc179
Focus-Sash <tenma.quo.ast@gmail.com>
2023-03-05 13:00:43 +0900

    Add empty endpoint /subscriptions

--- rust/tests/health_check.rs
+++ rust/tests/health_check.rs
@@ -72,7 +72,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
         assert_eq!(
             400,
             response.status().as_u16(),
-            "The API did not fail with 400 Bad Request when the payload was {}.",
+            "\nThe API did not fail with 400 Bad Request when the payload was {}.",
             error_message
         );
     }
