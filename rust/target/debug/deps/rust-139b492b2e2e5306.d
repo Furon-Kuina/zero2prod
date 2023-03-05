6ea8e145c815a90df0d533a6a4eb2f13896255fa
Focus-Sash <tenma.quo.ast@gmail.com>
2023-03-05 09:42:15 +0900

    Add auto-format to CI

---
+++ rust/target/debug/deps/rust-139b492b2e2e5306.d
@@ -0,0 +1,8 @@
+/home/meip/zero2prod/rust/target/debug/deps/rust-139b492b2e2e5306.rmeta: src/main.rs Cargo.toml
+
+/home/meip/zero2prod/rust/target/debug/deps/rust-139b492b2e2e5306.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
---
+++ rust/target/debug/deps/rust-5a05acc1ed7e1242.d
@@ -0,0 +1,5 @@
+/home/meip/zero2prod/rust/target/debug/deps/rust-5a05acc1ed7e1242.rmeta: src/main.rs
+
+/home/meip/zero2prod/rust/target/debug/deps/rust-5a05acc1ed7e1242.d: src/main.rs
+
+src/main.rs:
