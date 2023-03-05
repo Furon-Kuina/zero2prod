5289ac11a007556e100879c3f7b4a7fdb397628e
Focus-Sash <tenma.quo.ast@gmail.com>
2023-03-05 17:51:12 +0900

    Create database table & change CI configuration

--- rust/tests/health_check.rs
+++ rust/tests/health_check.rs
@@ -34,13 +34,12 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     let app_address = spawn_app();
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_string = configuration.database.connection_string();
-
-    // connect to PostgresQL, not doing any job for now
-    let connection = PgConnection::connect(&connection_string)
+    let mut connection = PgConnection::connect(&connection_string)
         .await
         .expect("Failed to connect to Postgres.");
     let client = reqwest::Client::new();
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
     let response = client
         .post(&format!("{}/subscriptions", &app_address))
         .header("Content-Type", "application/x-www-form-urlencoded")
@@ -51,6 +50,14 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
     // Assert
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription");
+
+    assert_eq!(saved.email, "tenma.edamura@gmail.com");
+    assert_eq!(saved.name, "tenma edamura");
 }
 
 #[tokio::test]
